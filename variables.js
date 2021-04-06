cdb634575d60bb27896a10c96c274676fea1e93f
lennert-mdware <78749405+lennert-mdware@users.noreply.github.com>
2021-04-06 13:12:25 +0200

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,104 @@
+const vars = {
+    /**
+     * Buttons met hoge prioriteit, of zaken die het meest in het oog moeten springen
+     */
+    
+    "btn-main--background": "#4062F6",
+    "btn-main--text": "#FFFFFF",
+    
+    /**
+     * Buttons met normale prioriteit, op lichte achtergronden
+     */
+    "btn-second--background": "#CFD7F7",
+    "btn-second--text": "#627EF8",
+    
+    /**
+     * Buttons met lage prioriteit
+     * Voor voor navigatie door schermen (back-button), next-button in lijsten, of openen van collapses
+     */
+    "btn-third--background": "#E3E8FE",
+    "btn-third--text": "#627EF8",
+    "btn-third--icon": "#95A8FA",
+    
+    
+    /**
+     * Outlined buttons, voorlopig enkel orderbuttons
+     */
+    "btn-outline--border": "#E3E8FE",
+    "btn-outline--background": "#FFFFFF",
+    "btn-outline--text": "#627EF8",
+    "btn-outline--icon": "#627EF8",
+    
+    "btn-outline-active--border": "#627EF8",
+    "btn-outline-active--background": "#E3E8FE",
+    "btn-outline-active--text": "#627EF8",
+    "btn-outline-active--icon": "#627EF8",
+    
+    /**
+     * Buttons voor destructieve acties
+     */
+    "btn-warning--background": "#FEE3E3",
+    "btn-warning--text": "#F86262",
+    "btn-warning--icon": "#FBA2A2",
+    
+    /**
+     * Borders
+     */
+    "border-main": "#ECECEC",
+    "border-on-background": "#CFD7F7",
+    
+    /**
+     * Background
+     */
+    "background--main": "#4062f6",
+    "background--modal": "#00000030", // met opacity
+    "background--modal-layover": "#00000030", // met opacity
+    
+    /**
+     * Lists
+     */
+    "lists--background": "#FFFFFF",
+    "lists--text": "#707070",
+    
+    "lists-active--background": "#627EF8",
+    "lists-active--text": "#FFFFFF",
+    
+    /**
+     * Text & titles
+     */
+    "text": "#898989", // body text
+    "title-main": "#4062F6", // grote opvallende titels
+    "title-secondary": "#627EF8", // subtitels
+    "title-third": "#707070", // normale titels
+    
+    /**
+     * Cards
+     */
+    "card-main--background": "#E3E8FE",
+    "card-secondary--background": "#F8FAFF",
+    
+    /**
+     * Components
+     */
+    "payment-btn--background": "#4062F6",
+    "payment-btn--background": "#FFFFFF",
+    
+    // Vlak dat naar boven geswiped wordt met cart
+    "payment-float--background": "#E3E8FE",
+    "payment-float-active--background": "#FFFFFF",
+    
+    // Vlak boven payment button voor tips
+    "tips-float--background": "#E3E8FE",
+    
+    // Swipable tabs
+    "tabs--background": "#E3E8FE",
+    "tabs--text": "#627EF8",
+    "tabs-active--background": "#4062F6",
+    "tabs-active--text": "#FFFFFF",
+    
+    "tabs--icon": "#4062F6",
+    
+    // Order summary
+    "summ--background": "#E3E8FE",
+    "summ-children--background": "#F8FAFF",
+}
